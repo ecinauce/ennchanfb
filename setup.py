720988c2aac397233b61d865613953179ac03001
ecinauce <nicololynsuecuacion@gmail.com>
2018-03-10 15:21:34 +0800

    [BUGFIX] Added in the requirements files to make the basic app running.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='EnnchanFB',
+      version='0.0',
+      description='The Ennchan Facebook Messenger bot.',
+      author='Ennchan',
+      author_email='nicololynsuecuacion@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      packages=find_packages(),
+      install_requires=['Flask'],
+     )
